4aa0abdb235fd7406df45175c76018ee1b94c3d0
ngasaraphael <66059003+ngasaraphael@users.noreply.github.com>
2021-08-02 17:13:25 +0200

    Add files via upload

---
+++ BAYA/main.js
@@ -0,0 +1,13 @@
+
+// Get the current year for the copyright
+	$('#year').text(new Date().getFullYear());
+
+	//what we do slider
+	$('.slider').slick({
+		infinite: true,
+		slideToShow: 1,
+		slideToScroll: 1,
+	});
+
+
+
